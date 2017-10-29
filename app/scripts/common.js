060435cef17b28cca9f53f6850cb730ce76fd0b8
Olga <tereshkoolga54@gmail.com>
2017-10-29 23:09:05 +0400

    almost done

--- app/scripts/common.js
+++ app/scripts/common.js
@@ -1,1 +1,55 @@
+(function() {
+    'use strict';
 
+    window.onload = function() {
+
+        function eventBinder() {
+            document.querySelector('.js-show-popup').addEventListener("click", function(){
+                showPopup();
+            });
+            document.querySelector('.js-close-popup').addEventListener("click", function(){
+                hidePopup();
+            });
+            document.querySelector('.js-hamburger').addEventListener("click", function(){
+                switchMobileMenu(this);
+            });
+        }
+
+        function  showPopup(){
+            document.querySelector('.js-popup-trial').style.display="block"
+        }
+
+        function  hidePopup(){
+            document.querySelector('.js-popup-trial').style.display="none"
+        }
+
+        function clickTabs() {
+            var setTabsLinks = document.querySelectorAll('.js-features-list li'),
+                setTabsContent = document.querySelectorAll('.js-features-blocks .features-content');
+
+            for (var i=0; i < setTabsLinks.length; i++) {
+                setTabsLinks[i].onclick = function () {
+                    for (var i=0; i < setTabsLinks.length; i++) {
+                        setTabsLinks[i].classList.remove('active');
+                    }
+                    for (var i=0; i < setTabsContent.length; i++) {
+                        setTabsContent[i].classList.remove('active-content');
+                    }
+
+                    var currentLink = this.getAttribute('data-tab');
+                    this.classList.add("active");
+
+                    document.querySelector('[data-content=' + currentLink + ']').classList.add("active-content");
+                };
+            }
+        }
+
+        function switchMobileMenu(self) {
+            self.classList.toggle('open');
+        }
+
+        eventBinder();
+        clickTabs();
+    }
+
+})();
